7e01a6c09fcbe4241de6aadacde94473af1401b4
TancelinMazzotti <tancelin.mazzotti@gmail.com>
2016-11-18 14:58:22 +0100

    Finalisation fonction charger

--- donnees_partie.c
+++ donnees_partie.c
@@ -41,7 +41,7 @@ int sauvegarde(DonneesPartie * donnes_a_sauvegarder)
     }
 
     else
-    { 
+    {
         printf("Erreur ouverture de fichier sauvegarde\n");
         return 0;
     }
@@ -51,11 +51,28 @@ int charger(DonneesPartie * donnes_a_charger)
 {
     FILE * fichier_chargement = NULL;
     fichier_chargement = fopen("partie.save","r");
-
+		int x,y,i;
     if (fichier_chargement != NULL)
     {
-        fclose(fichier_chargement);
-        return 1;
+			for (y = 0; y < TAILLE_PLATEAU; y++)
+			{
+				for (x = 0; x < TAILLE_PLATEAU; x++)
+				{
+					donnes_a_charger->plateau[x][y] = char_to_int(fgetc(fichier_chargement)); // chargement plateau
+				}
+				fgetc(fichier_chargement); // Passage de ligne dans le fichier
+			}
+			donnes_a_charger->num_joueur = char_to_int(fgetc(fichier_chargement)); // chargement numero de joueur
+			fgetc(fichier_chargement); // Passage de ligne dans le fichier
+			donnes_a_charger->nombre_joueur = char_to_int(fgetc(fichier_chargement)); //chargement nombre de joueur
+			fgetc(fichier_chargement); // Passage de ligne dans le fichier
+			for (i = 0; i < 4; i++)
+			{
+				donnes_a_charger->classement[i] = char_to_int(fgetc(fichier_chargement)); // chargement classement
+			}
+
+      fclose(fichier_chargement);
+      return 1;
     }
 
     else
